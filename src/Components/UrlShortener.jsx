7af56fbfa94c631bed01ed779cf093a86f666859
mango_gills <juzzgil21@gmail.com>
2022-08-22 16:20:34 +0800

    fix mobile layout - url shortener component

--- src/Components/UrlShortener.jsx
+++ src/Components/UrlShortener.jsx
@@ -3,30 +3,33 @@ import React from "react";
 const UrlShortener = () => {
   return (
     <section className="relative">
-      <div className="relative mx-auto laptop:max-w-7xl">
+      <div className="relative mx-auto mt-12 tablet:mt-0 laptop:max-w-7xl">
         <div
-          className="flex w-full py-8 bg-[#3b3054] bg-cover bg-no-repeat rounded-md items-center justify-center"
+          className="flex flex-col tablet:flex-row py-4 w-full tablet:py-8 bg-[#3b3054] bg-cover bg-no-repeat rounded-md items-center justify-center"
           style={{ backgroundImage: "url(/bg-shorten-desktop.svg)" }}
         >
           <input
             type="text"
             placeholder="Shorten a link here..."
-            className="w-[80%] my-4 mr-4 h-12 rounded-md py-2 px-5"
+            className="w-[80%] my-4 tablet:mr-4 h-12 rounded-md py-2 px-5"
           />
-          <button className="rounded-md bg-[#2acfcf] text-white text-normal font-bold px-4 py-[.8rem]">
+          <button
+            className="rounded-md bg-[#2acfcf] text-white mt-4 tablet:mt-0 text-xl tablet:text-sm font-bold px-4 py-[.9rem]
+          w-[80%] mx-auto tablet:mx-0 tablet:w-36"
+          >
             Shorten It!
           </button>
         </div>
 
         <div>
           <ul className="w-full my-4">
-            <li className="flex items-center justify-between p-4 bg-white rounded-md">
-              <p className="text-gray-700">
+            <li className="flex flex-col items-start justify-between px-8 py-4 bg-white rounded-md tablet:items-center tablet:flex-row">
+              <p className="w-full pb-3 mb-2 text-gray-700 border-b-2 tablet:mb-0 tablet:pb-0 tablet:border-b-0">
                 https://www.frontendmentor.io/home
               </p>
-              <div className="flex items-center">
+              <div className="flex flex-col items-start w-full space-y-2 tablet:space-y-0 tablet:w-[300px] tablet:items-center tablet:flex-row">
                 <p className="mr-4 text-[#2acfcf]">https://www.asdfdfme</p>
-                <button className="bg-[#2acfcf] px-5 py-2 rounded-md text-white text-xs">
+                <button className="bg-[#2acfcf] px-5 py-2 rounded-md text-white text-xs w-full tablet:w-20">
                   Copy
                 </button>
               </div>
